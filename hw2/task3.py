8bf14606743b320d276e69b61883bb97eb5ca295
Артём Киорогло <p.grushev@MacBook-Air-Artem.local>
2023-09-17 15:33:36 +0300

    fix comms in task3

--- hw2/task3.py
+++ hw2/task3.py
@@ -22,59 +22,59 @@ def reverse_list(head: ListNode) -> ListNode:
     return last
     
     
-# # tests for task3
+ # tests for task3
 
-# def get_values_from_linked_list(head: ListNode):
-#     string = ""
-#     while head:
-#         string += str(head.val) + " "
-#         head = head.next
+def get_values_from_linked_list(head: ListNode):
+    string = ""
+    while head:
+        string += str(head.val) + " "
+        head = head.next
         
-#     return string[0:len(string) - 1]
+    return string[0:len(string) - 1]
     
-# # test1
+ # test1
 
-# #input
-# l5_in_test1 = ListNode(5, None)
-# l4_in_test1 = ListNode(4, l5_in_test1)
-# l3_in_test1 = ListNode(3, l4_in_test1)
-# l2_in_test1 = ListNode(2, l3_in_test1)
-# l1_in_test1 = ListNode(1, l2_in_test1)
+#input
+l5_in_test1 = ListNode(5, None)
+l4_in_test1 = ListNode(4, l5_in_test1)
+l3_in_test1 = ListNode(3, l4_in_test1)
+l2_in_test1 = ListNode(2, l3_in_test1)
+l1_in_test1 = ListNode(1, l2_in_test1)
 
-# #output
-# l2_out_test1 = ListNode(2, None)
-# l3_out_test1 = ListNode(3, l2_out_test1)
-# l4_out_test1 = ListNode(4, l3_out_test1)
-# l5_out_test1 = ListNode(5, l4_out_test1)
-# l1_out_test1 = ListNode(1, l5_out_test1)
+#output
+l2_out_test1 = ListNode(2, None)
+l3_out_test1 = ListNode(3, l2_out_test1)
+l4_out_test1 = ListNode(4, l3_out_test1)
+l5_out_test1 = ListNode(5, l4_out_test1)
+l1_out_test1 = ListNode(1, l5_out_test1)
 
-# print(get_values_from_linked_list(reverse_list(l1_in_test1)) == get_values_from_linked_list(l1_out_test1))
-
-
-# # test2
-# #input - output
-# l1_in_test2 = ListNode(5, None)
-
-# print(get_values_from_linked_list(reverse_list(l1_in_test2)) == get_values_from_linked_list(l1_in_test2))
+print(get_values_from_linked_list(reverse_list(l1_in_test1)) == get_values_from_linked_list(l1_out_test1))
 
 
-# # test3
-# #input - output
-# l2_in_test3 = ListNode(2, None)
-# l1_in_test3 = ListNode(1, l2_in_test3)
+# test2
+#input - output
+l1_in_test2 = ListNode(5, None)
 
-# print(get_values_from_linked_list(reverse_list(l1_in_test3)) == get_values_from_linked_list(l1_in_test3))
+print(get_values_from_linked_list(reverse_list(l1_in_test2)) == get_values_from_linked_list(l1_in_test2))
+
+
+# test3
+#input - output
+l2_in_test3 = ListNode(2, None)
+l1_in_test3 = ListNode(1, l2_in_test3)
+
+print(get_values_from_linked_list(reverse_list(l1_in_test3)) == get_values_from_linked_list(l1_in_test3))
 
 
 # # test4
- #input
- l3_in_test4 = ListNode(3, None)
- l2_in_test4 = ListNode(2, l3_in_test4)
- l1_in_test4 = ListNode(1, l2_in_test4)
+#input
+l3_in_test4 = ListNode(3, None)
+l2_in_test4 = ListNode(2, l3_in_test4)
+l1_in_test4 = ListNode(1, l2_in_test4)
 
- #output
- l2_out_test4 = ListNode(2, None)
- l3_out_test4 = ListNode(3, l2_out_test4)
- l1_out_test4 = ListNode(1, l3_out_test4)
+#output
+l2_out_test4 = ListNode(2, None)
+l3_out_test4 = ListNode(3, l2_out_test4)
+l1_out_test4 = ListNode(1, l3_out_test4)
 
- print(get_values_from_linked_list(reverse_list(l1_in_test4)) == get_values_from_linked_list(l1_out_test4))
+print(get_values_from_linked_list(reverse_list(l1_in_test4)) == get_values_from_linked_list(l1_out_test4))
